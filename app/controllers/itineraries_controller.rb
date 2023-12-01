545a8cc639938bf07e1651f76fc0e8cbf4f98d84
Cesar Alexandre <cesaralexandreprofissional@gmail.com>
2023-12-01 11:26:56 +0200

    Itineraries and few additionals things on the excursion-index

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -15,7 +15,11 @@ class ItinerariesController < ApplicationController
   def create
     @itinerary = Itinerary.new(itinerary_params)
     @itinerary.user = current_user
+    excursion_ids = params[:itinerary_excursions][:excursion_ids].reject(&:empty?)
     if @itinerary.save
+      excursion_ids.each do |excursion|
+        ItineraryExcursion.create(excursion_id: excursion, itinerary_id: @itinerary.id)
+      end
       redirect_to itineraries_path, notice: 'Itinerary successfully created'
     else
       render :new, status: :unprocessable_entity
@@ -27,8 +31,9 @@ class ItinerariesController < ApplicationController
   end
 
   def update
+    @itinerary = Itinerary.find(params[:id])
     if @itinerary.update(itinerary_params)
-      redirect_to excursion_itinerary_path, notice: 'Itinerary successfully updated'
+      redirect_to itinerary_path, notice: 'Itinerary successfully updated'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -37,12 +42,16 @@ class ItinerariesController < ApplicationController
   def destroy
     @itinerary = Itinerary.find(params[:id])
     @itinerary.destroy
-    head :no_content
+    respond_to do |format|
+      format.html { redirect_to itineraries_path, notice: "Itinerary was successfully deleted." }
+      format.json { head :no_content }
+    end
   end
 
   private
 
   def itinerary_params
-    params.require(:itinerary).permit(:title, :description, :excursion_ids, :user_id, :start_time, :end_time)
+    params.require(:itinerary).permit(:title, :description, :excursion_ids, :user_id, :start_time, :end_time,
+                                      itinerary_excursions: [:excursion_id])
   end
 end
